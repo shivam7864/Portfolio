cb41ee31c8b9a6f0aa9be818308a7f599052b3b8
Shivam Tiwari <shivamtiwari7864@gmail.com>
2025-05-20 20:13:59 +0530

    Updated Resume Link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1rXlDD5dBkVatyATNJCcvWcb7yaXouIbu/view?usp=sharing"
+            href="https://drive.google.com/file/d/1IOvpDI_lOpCnHZAAsu2ivCLKLb5vlNe1/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
